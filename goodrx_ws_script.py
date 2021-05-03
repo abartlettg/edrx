337bdd94f04e933bb558488d84bcfadc7783c0ff
Adriana Bartlett Gray <77497942+abartlettg@users.noreply.github.com>
2021-05-03 16:14:37 -0400

    Retrieval of Pharmacy and Price Data Added

--- goodrx_ws_script.py
+++ goodrx_ws_script.py
@@ -11,21 +11,10 @@ import time
 PATH = "C:\\Users\\adria\\chromedriver_win32\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+###### TO BE LOOPED THROUGH 3 DIFFERENT MEDICATION TYPES AND POSSIBLY VARIATIONS IN DOSAGE (TBD) ######
 driver.get("https://www.goodrx.com/sildenafil?dosage=100mg&form=tablet&label_override=sildenafil&quantity=30&sort_type=popularity")
-#print(driver.title)
 
-#### WILL LIKELY USE 3 START URLS INSTEAD ####
-# enter_rx = driver.find_element_by_name("search-drugs")
-# enter_rx.send_keys("sildenafil")
-# Seems to get confused here w/dropdown list
-# enter_rx.send_keys(Keys.TAB)
-#enter_rx.send_keys(Keys.ENTER)
-#enter_rx.send_keys = driver.find_element_by_?????
-#enter_rx_btn.click()
-#### REMOVE WHEN CLEANING UP CODE ####
-
-#### Need to close first popup ####
-
+# Closing initial popup
 try:
 	ok_btn = WebDriverWait(driver, 10).until(
 		EC.presence_of_element_located((By.CLASS_NAME, "okButton-hyrC_"))
@@ -34,17 +23,8 @@ try:
 except:
 	print("OK Button Not Detected")
 
-
-#try:
-#	set_loc_btn = WebDriverWait(driver, 10).until(
-#		EC.presence_of_element_located((By., "Button"))
-#		)
-#	set_loc_btn.click()
-#except:
-#	print("Set Location Not Detected")
-
-#driver.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
-
+###### TO BE LOOPED THROUGH A LIST OF ZIP CODES TO BE ANALYZED ######
+# Setting Location by Zip Code
 set_loc_btn = driver.find_element_by_class_name("btn-3E6az")
 set_loc_btn.click()
 
@@ -57,14 +37,30 @@ try:
 except:
 	print("Enter Zip Did Not Work")
 
-###### WORKS TO THIS POINT... UPDATE GITHUB ######
+# Gathering Pharmacy and Rx Price Data
+
+# Note that this NEEDS to be an Implicit Wait because the
+# EC are met by data displayed from the PREVIOUS zip code 
+# and therefore data related to the previous zip code will 
+# be retrieved unless there is a hard-coded wait time
+time.sleep(10)
+
+pharm_name_lis = driver.find_elements_by_class_name("goldAddUnderline-1CwEN")
+for i in pharm_name_lis:
+	print(i.text)
+
+# Note that the full price is not available for every pharmacy
+# Need to figure out how to deal with this... need to index somehow
+rx_full_price_lis = driver.find_elements_by_class_name("retailPriceStrikeSavings-1p-dB")
+for i in rx_full_price_lis:
+	print(i.text)
+
+rx_disc_price_lis = driver.find_elements_by_class_name("display-2zakM")
+for i in rx_disc_price_lis:
+	print(i.text)
 
 
-#set_loc = driver.find_element_by_class("btn_3E6az")
-#set_loc.send_keys("10025")
-#set_loc.send_keys(Keys.RETURN)
 
-#print(driver.page_source)
 
 #time.sleep(5)
 
